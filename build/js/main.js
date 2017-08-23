507008ccfa19666ea309716d3210a8a940bbfa3d
BobrovnikArtyom350504 <artyonn@gmail.com>
2017-08-23 08:27:02 +0300

    build

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var $ = require('jquery');
+var slick = require('slick-carousel');
+$('.test').slick();
+'use strict';
+
+var _slickCarousel = require('slick-carousel');
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _jquery2.default)('.test').slick();
+//# sourceMappingURL=main.js.map
--- src/js/slider.js
+++
@@ -1,3 +0,0 @@
-import { slick } from 'slick-carousel';
-import $ from 'jquery';
-$('.test').slick();
